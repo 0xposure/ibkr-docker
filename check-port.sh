03acbc1cb68944b6a08940f3dec833fd0acf578a
Qijie Pan <qiijie.pan@hulu.com>
2023-07-12 23:01:28 +0800

    check port auto

--- check-port.sh
+++ check-port.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
+if [[ ${GATEWAY_OR_TWS@L} = "gateway" ]]; then
+    if [[ ${IBC_TradingMode:-live} = "live" ]]; then
+        # IBGateway Live
+        port=4001
+    else
+        # IBGateway Paper
+        port=4002
+    fi
+elif [[ ${IBC_TradingMode:-live} = "live" ]]; then
+    # TWS Live
+    port=7496
+else
+    # TWS Paper
+    port=7497
+fi
+
 # Check if both ports are open
-if lsof -i :8888 | grep -q LISTEN && lsof -i :4002 | grep -q LISTEN; then
+if lsof -i :8888 | grep -q LISTEN && lsof -i :${port} | grep -q LISTEN; then
   exit 0
 else
   exit 1
